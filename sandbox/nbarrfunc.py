eaa4424a87567a311f9916debf9d635d3376fe22
Graham Markall <graham.markall@continuum.io>
2015-06-08 17:14:48 +0100

    Prevent compiled code getting GC'd

--- sandbox/nbarrfunc.py
+++ sandbox/nbarrfunc.py
@@ -1,8 +1,12 @@
 from numba import types, compiler
 from dynd import nd, ndt
 
+# Shove all compilation results in a global list to prevent them getting deleted!
+compile_results = []
+
 def arrfunc(f):
     res = compiler.compile_isolated(f, (types.int32, types.int32))
+    compile_results.append(res)
     ptr = res.library.get_pointer_to_function(res.fndesc.llvm_func_name)
     proto = ndt.type('(int, int) -> int')
     return nd.arrfunc(ptr, proto)
